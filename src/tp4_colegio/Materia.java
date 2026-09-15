8ff5140a8f775a069a5e8804e078cb19dee9f88c
magallanesfranco694-creator <magallanesfranco694@gmail.com>
2026-09-15 20:22:20 -0300

    Update Materia.java
    clase materia

--- src/tp4_colegio/Materia.java
+++ src/tp4_colegio/Materia.java
@@ -9,9 +9,63 @@ package tp4_colegio;
  * @author Grupo9
  */
 public class Materia {
+private int idMateria;
+private String nombre;
+private int anio;
 
-    public Materia() {
-
+    public Materia(int idMateria, String nombre, int anio) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
     }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.idMateria;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Materia other = (Materia) obj;
+        return this.idMateria == other.idMateria;
+    }
+
+
+
 
 }
