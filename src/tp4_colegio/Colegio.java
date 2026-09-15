1131fee47b83ed389b8269d94cdebdfd762d97e6
Omar Matijas <omarmatijas@gmail.com>
2026-09-15 20:24:51 -0300

    clase colegio

--- src/tp4_colegio/Colegio.java
+++ src/tp4_colegio/Colegio.java
@@ -14,6 +14,22 @@ public class Colegio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Materia web2 = new Materia(1, "WEB 2", 2);
+        Materia mat1 = new Materia(2, "Matemáticas 1", 1);
+        Materia lab1 = new Materia(3, "Laboratorio 1", 1);
+        Alumno a1 = new Alumno(1001, "Lopez", "Martin");
+        Alumno a2 = new Alumno(1002, "Martinez", "Brenda");
+
+        a1.agregarMateria(web2);
+        a1.agregarMateria(mat1);
+        a1.agregarMateria(lab1);
+        System.out.println("Inscriptas de "+a1.getApellido()+": "+ a1.cantidadMaterias());
+
+        a2.agregarMateria(web2);
+        a2.agregarMateria(mat1);
+        a2.agregarMateria(lab1);
+        a2.agregarMateria(lab1);
+        System.out.println("Inscriptas de "+a2.getApellido()+": "+ a2.cantidadMaterias());
 
     }
 
